60c8a31f683c6824ba4a9f84029df5a05c56260d
RyanG <decatf@gmail.com>
2015-07-08 16:44:58 -0400

    Update build config
     - Add overlay to disable system update service
     - Add custom toolchain flags
     - Add misc build packages

--- config/common.mk
+++ config/common.mk
@@ -5,9 +5,12 @@ PRODUCT_COPY_FILES += \
     vendor/decatf/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
     vendor/decatf/prebuilt/bin/blacklist:system/addon.d/blacklist
 
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/decatf/overlay/common
+
 # Init script file with custom extras
 PRODUCT_COPY_FILES += \
- vendor/custom/prebuilt/etc/init.local.rc:root/init.custom.rc
+	vendor/decatf/prebuilt/etc/init.local.rc:root/init.custom.rc
 
 # init.d support
 PRODUCT_COPY_FILES += \
@@ -29,11 +32,19 @@ PRODUCT_PACKAGES += \
 #    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
 
 # Stagefright FFMPEG plugin
-#PRODUCT_PACKAGES += \
-#    libstagefright_soft_ffmpegadec \
-#    libstagefright_soft_ffmpegvdec \
-#    libFFmpegExtractor \
-#    libnamparser
+PRODUCT_PACKAGES += \
+    libstagefright_soft_ffmpegadec \
+    libstagefright_soft_ffmpegvdec \
+    libFFmpegExtractor \
+    media_codecs_ffmpeg.xml
+
+# aosp master libavc stagefright
+PRODUCT_PACKAGES += \
+	libstagefright_soft_avcdec
+
+PRODUCT_PACKAGES += \
+	liblog-benchmarks \
+	bionic-benchmarks
 
 # Chromium Prebuilt
 ifeq ($(USE_PREBUILT_CHROMIUM),1)
@@ -41,3 +52,20 @@ ifeq ($(USE_PREBUILT_CHROMIUM),1)
 endif
 
 -include vendor/decatf/sepolicy/sepolicy.mk
+
+# Android system toolchain
+# TARGET_GCC_VERSION_EXP := 4.8
+# TARGET_TOOLCHAIN_ROOT := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/linaro-arm-eabi-$(TARGET_GCC_VERSION)
+# TARGET_TOOLS_PREFIX := $(TARGET_TOOLCHAIN_ROOT)/bin/arm-linux-gnueabi-
+
+# Kernel toolchain
+# TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-5.1
+# TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03
+
+# Add backup-tool.sh to install script
+BACKUP_TOOL := true
+
+# Inherit sabermod vendor
+SM_VENDOR := vendor/sm
+include $(SM_VENDOR)/Main.mk
